ce8566e89d471e47ee103c1d565207eb9e3e57d5
se4oev <p.karpenko26@gmail.com>
2021-03-22 22:28:18 +0300

    Запрет ввода текста в текстфилд, слушатели на кнопки 0 - 9 (включая numpad)

--- src/main/java/CalculatorRootPane.java
+++ src/main/java/CalculatorRootPane.java
@@ -1,9 +1,13 @@
 import common.IFxmlPane;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+
 
 /**
  * Created by taranenko on 28.01.2021
@@ -13,6 +17,28 @@ public class CalculatorRootPane implements IFxmlPane {
 
     @FXML
     private AnchorPane rootPane;
+    @FXML
+    private TextField textField;
+    @FXML
+    private Button btnZero;
+    @FXML
+    private Button btnOne;
+    @FXML
+    private Button btnTwo;
+    @FXML
+    private Button btnThree;
+    @FXML
+    private Button btnFour;
+    @FXML
+    private Button btnFive;
+    @FXML
+    private Button btnSix;
+    @FXML
+    private Button btnSeven;
+    @FXML
+    private Button btnEight;
+    @FXML
+    private Button btnNine;
 
     @FXML
     public void initialize(){
@@ -22,10 +48,95 @@ public class CalculatorRootPane implements IFxmlPane {
 
             }
         });
+
+        rootPane.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (isNumber(event.getText())) {
+                    textField.appendText(event.getText());
+                }
+            }
+        });
+
+        textField.setEditable(false);           //Запрещаем ввод текста в textField
+
+        btnZero.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                textField.appendText("0");
+            }
+        });
+        btnOne.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                textField.appendText("1");
+            }
+        });
+        btnTwo.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                textField.appendText("2");
+            }
+        });
+        btnThree.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                textField.appendText("3");
+            }
+        });
+        btnFour.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                textField.appendText("4");
+            }
+        });
+        btnFive.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                textField.appendText("5");
+            }
+        });
+        btnSix.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                textField.appendText("6");
+            }
+        });
+        btnSeven.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                textField.appendText("7");
+            }
+        });
+        btnEight.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                textField.appendText("8");
+            }
+        });
+        btnNine.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                textField.appendText("9");
+            }
+        });
+    }
+
+    private boolean isNumber(String text) {
+        try {
+            Integer.parseInt(text);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public Pane getRootPane() {
         return rootPane;
+    }
+
+    public void numFromKeyboard(String num) {
+        textField.appendText(num);
     }
 }
